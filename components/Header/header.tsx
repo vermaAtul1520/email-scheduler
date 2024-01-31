249a40adae1d3707289c66f6b97156fa8168765f
Atul Verma <atulverma@Atuls-MacBook-Air.local>
2024-02-01 01:53:02 +0530

    debounce function added on search

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,16 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState ,useCallback} from 'react'
 import style from './Header.module.css'
 import { FaSearch } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 import { usePopupContext } from "@/context/popupcontext"
 
 const Header = () => {
-    const { openPopup } = usePopupContext();
-    
+    const { openPopup,displaySearch } = usePopupContext();
+    const [searchText, setSearchText] = useState('');
+
+    const debounceFunction = (func: any, delay: any) => {
+        let timer: any;
+        return function () {
+            let self = this;
+            let args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                func.apply(self, args);
+            }, delay);
+        };
+    };
+
+    const debounceSearch = useCallback(
+        debounceFunction((val: any) => {
+            displaySearch(val);
+        }, 500),
+        [],
+    );
+
+    const handleSearch = async (val: string) => {
+        setSearchText(val);
+        debounceSearch(val);
+    };
+
     return (
         <div className={style.mainContainer}>
             <div className={style.searchBar}>
-                <input className={style.searchInput} type="text" placeholder="Search" />
+                <input 
+                onChange={(e)=>{
+                    handleSearch(e.target.value);
+                }}
+                 className={style.searchInput} type="text" placeholder="Search" />
                 <FaSearch />
             </div>
             <button className={style.button}
